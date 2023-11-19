f8a6c5f582f8dc50a90c1f61090508e41a7ce303
Hieliksuslik <hieliksuslik@gmail.com>
2023-11-19 01:46:58 -0600

    Implemented first iteration of file IO

--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ using System.Globalization;
 using HtmlAgilityPack;
 using System.Linq;
 using System.Diagnostics;
+using System.Threading;
 
 namespace WebScraper {
     
     class Program {
         public class CardInfo {
+        public string CardName {get; set; }
         public string SellerSite {get; set; }
         public string Edition {get; set; }
         public string Rarity {get; set; }
@@ -18,7 +20,8 @@ namespace WebScraper {
         public string ListingLink {get; set; }
         public string Price {get; set; }
 
-        public CardInfo(IEnumerable<HtmlNode> CardResult){
+        public CardInfo(IEnumerable<HtmlNode> CardResult, string cardname){
+            CardName = cardname;
             SellerSite = CardResult.ElementAt(0).InnerText.Replace("\n", "");
             Edition = CardResult.ElementAt(1).InnerText;
             Rarity = CardResult.ElementAt(2).InnerText;
@@ -28,23 +31,36 @@ namespace WebScraper {
         }
 
         override public string ToString(){
-            return $"Seller: \t\t{SellerSite}\nEdition: \t\t{Edition}\nRarity: \t\t{Rarity}\nCondition: \t\t{Condition}\nListing Link: \t\t{ListingLink}\nPrice: \t\t\t{Price}";
+            return $"CardName: \t\t\t{CardName}\nSeller: \t\t\t{SellerSite}\nEdition: \t\t\t{Edition}\nRarity: \t\t\t{Rarity}\nCondition: \t\t\t{Condition}\nListing Link: \t\t{ListingLink}\nPrice: \t\t\t\t{Price}";
         }
     }
     
         static int Main(string[] args){
-            var html = @"https://yugiohprices.com/card_price?name=Dark%20Magician";
             HtmlWeb web = new HtmlWeb();
 
-            var htmlDoc = web.Load(html);
+            string[] cardList = File.ReadAllLines("./MockData.txt");
+            List<CardInfo> shoppingList = new List<CardInfo>();
 
-            var cardResult = htmlDoc.DocumentNode
+            foreach (string cardName in cardList){
+                Thread.Sleep(100);
+                string html = @$"https://yugiohprices.com/card_price?name={cardName.Replace(" ", "%20")}";
+                var htmlDoc = web.Load(html);
+
+                var cardResult = htmlDoc.DocumentNode
                 .SelectSingleNode("//table[@id='other_merchants']")
                 .SelectSingleNode("//tr[@id='']")
                 .Descendants("p");
 
-            CardInfo card1 = new CardInfo(cardResult);
-            Console.WriteLine(card1);
+                shoppingList.Add(new CardInfo(cardResult, cardName));
+            }
+            
+            using(TextWriter writer = new StreamWriter("./ShoppingFile.txt")){
+                foreach (var item in shoppingList){
+                    writer.WriteLine(item);
+                }
+            }
+            
+            // File.WriteAllLines("./ShoppingFile.txt", (IEnumerable<string>)shoppingList);
 
             return 0;
         }
